238d660c5954f053b35685621a82d2286642a1dc
Sneha Dhanekula <dhanekula.sneha@gmail.com>
2025-02-16 19:00:47 -0700

    all

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = ["Software Engineer", "Data Analyst", "Web Developer"];
+  const toRotate = ["Software Engineer", "FullStack Developer", "Web Developer"];
   const period = 2000;
 
   useEffect(() => {
