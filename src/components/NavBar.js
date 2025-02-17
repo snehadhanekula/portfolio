95134efcee533ae06b6064bfdf74ede7f384530f
snehadhanekula <65163692+snehadhanekula@users.noreply.github.com>
2025-02-16 17:09:06 -0700

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,103 +2,90 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useEffect, useState } from "react";
-import logo from '../assets/img/logo.svg'
-import navIcon1 from '../assets/img/nav-icon1.svg'
-import navIcon2 from '../assets/img/nav-icon2.svg'
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
-export default function NavBar(){
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
+export default function NavBar() {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50) {
-                setScrolled(true);
-            }
-            else {
-                setScrolled(false)
-            }
-        }
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-        window.addEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll);
 
-        return () => window.removeEventListener("scroll", onScroll);
-    }, [])
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
-    }
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
 
   return (
-		<Navbar expand="md" className={scrolled ? "scrolled" : ""}>
-			<Container>
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
+      <Container>
         <Navbar.Brand href="#home">
-        <img src={logo} alt="Logo" style={{ width: "95px" }} />
+          <img src={logo} alt="Logo" style={{ width: "95px" }} />
         </Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav">
-					<span className="navbar-toggler-icon"></span>
-				</Navbar.Toggle>
-				<Navbar.Collapse id="basic-navbar-nav">
-					<Nav className="me-auto">
-						<Nav.Link
-							href="#home"
-							className={
-								activeLink === "home"
-									? "active navbar-link"
-									: "navbar-link"
-							}
-							onClick={() => onUpdateActiveLink("home")}
-						>
-							Home
-						</Nav.Link>
-						<Nav.Link
-							href="#skills"
-							className={
-								activeLink === "skills"
-									? "active navbar-link"
-									: "navbar-link"
-							}
-							onClick={() => onUpdateActiveLink("skills")}
-						>
-							Skills
-						</Nav.Link>
-						<Nav.Link
-							href="#projects"
-							className={
-								activeLink === "projects"
-									? "active navbar-link"
-									: "navbar-link"
-							}
-							onClick={() => onUpdateActiveLink("projects")}
-						>
-							Experience
-						</Nav.Link>
-					</Nav>
-					<span className="navbar-text">
-						<div className="social-icon">
-							<a href="https://www.linkedin.com/in/snehadhanekula/">
-								<img src={navIcon1} alt="" />
-							</a>
-							<a href="https://github.com/snehadhanekula">
-								<img src={navIcon2} alt="" />
-							</a>
-							<a href="mailto:sdhanek1@asu.edu">
-								<img src={navIcon3} alt="" />
-							</a>
-						</div>
-						<button
-							className="vvd"
-							onClick={() =>
-								(window.location.href =
-									"/Sneha_Dhanekula_Resume_Portfolio.pdf")
-							}
-						>
-							My Resume
-						</button>
-					</span>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon" />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link
+              href="#home"
+              className={activeLink === "home" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={activeLink === "skills" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={activeLink === "projects" ? "active navbar-link" : "navbar-link"}
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Experience
+            </Nav.Link>
+          </Nav>
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/snehadhanekula/">
+                <img src={navIcon1} alt="" />
+              </a>
+              <a href="https://snehadhanekula.github.io/portfolio">
+                <img src={navIcon2} alt="" />
+              </a>
+              <a href="mailto:snehad2404@gmail.com">
+                <img src={navIcon3} alt="" />
+              </a>
+            </div>
+            <button
+              className="vvd"
+              onClick={() =>
+                (window.location.href = "/Sneha_Dhanekula_Resume_Portfolio.pdf")
+              }
+            >
+              My Resume
+            </button>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
+
